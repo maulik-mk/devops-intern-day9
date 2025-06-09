8b77fc019da1a9f7363fdb5906adaf96763280dc
Maulik Kadeval <maulikkadeval@Mauliks-MacBook-Air.local>
2025-06-09 10:28:06 +0530

    Server Fix

--- Backend/src/main.js
+++ Backend/src/main.js
@@ -18,7 +18,7 @@ app.use('/logs', logRoutes);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const publicPath = path.join(__dirname, '../public');
+const publicPath = path.join(__dirname, 'public');
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
