1563d154f4bcf2a8daab60badc8a83a9cd29603d
maulik-mk <22bt04048@gsfcuniversity.ac.in>
2025-06-09 09:54:44 +0530

    Update main.js

--- Backend/src/main.js
+++ Backend/src/main.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { createLogger } from '../services/logger.js';
 import logRoutes from '../routes/logRoutes.js';
 
@@ -11,6 +13,16 @@ const logger = createLogger(process.env.MONGO_URI);
 app.locals.logger = logger;
 
 app.use(cors());
+
 app.use('/logs', logRoutes);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const publicPath = path.join(__dirname, '../public');
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 export { app, logger };
